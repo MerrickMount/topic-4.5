6760c061c3b7bacd1c1b429cedbbd8c0b342efe4
Merrick Mount <339474587@GoTVDSB.ca>
2024-02-28 12:56:36 -0500

    1 thing done

--- Program.cs
+++ Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("This program is made for you to choose something to draw (Type the name of one and it will appear), here are your options:");
+            Console.WriteLine(" CokaCola");
             Console.WriteLine("          .e$.                           z$$e.d$$$.      z$b   z\r\n         d$’ .d                        .$$’ d’F ^*$$$e  z$’ $ .$\r\n       e$P   $%                       d$P .’  F    ‘$$’d$  .e$’\r\n      $$F                           .$$’  F  J       ‘$$z$$$’\r\n    .$$’   .$’3   .$’’  .$P $$  $$ 4$$’  $  4’       $$  .\r\n   .$$F   d$  4  d$ d$ z$’ J$%    4$$’   $.d’       $$  .’\r\n   $$P   $$ ‘.$z$$  ^ z$’ .$P    .$$F              $$’ .’\r\n  $$$   d$F  J $$F   z$$  $$ .   $$$   ze     .c  J$F z  .e.ze\r\n 4$$F   $$  4’ $$   z$$  $$’.’  d$$  d$’ $  z$’ $ $$ @  $$’.$F\r\n $$$   4$$.'’ 4$$ .$3$$.$$$e%   $$P J$P  P d$*  %$$$’  $$  $$\r\n $$$    $$*    $$$’ ^$$’'$$    4$$% $$’ . d$’ ‘$’$$   $$  $$\r\n $$$                           $$$  ‘   P4$P  z 4$F  $$’ J$% %\r\n '$$c          .e$$$$$$$$e     $$$F    $ $$  z’ $$  $$$ 4$$ P\r\n  ‘$$b.   .e$*’     ‘$$$$$$$   '$$$c.dP  $$$$’  $$$’$$$$$$$P\r\n    ‘*$$$*’           ‘          *$$*’   ‘$*    ‘$’ ^$* ‘$’\r\n");
+
+        }
+        public static void CokaCola()
+        {
+            Console.WriteLine("          .e$.                           z$$e.d$$$.      z$b   z\r\n         d$’ .d                        .$$’ d’F ^*$$$e  z$’ $ .$\r\n       e$P   $%                       d$P .’  F    ‘$$’d$  .e$’\r\n      $$F                           .$$’  F  J       ‘$$z$$$’\r\n    .$$’   .$’3   .$’’  .$P $$  $$ 4$$’  $  4’       $$  .\r\n   .$$F   d$  4  d$ d$ z$’ J$%    4$$’   $.d’       $$  .’\r\n   $$P   $$ ‘.$z$$  ^ z$’ .$P    .$$F              $$’ .’\r\n  $$$   d$F  J $$F   z$$  $$ .   $$$   ze     .c  J$F z  .e.ze\r\n 4$$F   $$  4’ $$   z$$  $$’.’  d$$  d$’ $  z$’ $ $$ @  $$’.$F\r\n $$$   4$$.'’ 4$$ .$3$$.$$$e%   $$P J$P  P d$*  %$$$’  $$  $$\r\n $$$    $$*    $$$’ ^$$’'$$    4$$% $$’ . d$’ ‘$’$$   $$  $$\r\n $$$                           $$$  ‘   P4$P  z 4$F  $$’ J$% %\r\n '$$c          .e$$$$$$$$e     $$$F    $ $$  z’ $$  $$$ 4$$ P\r\n  ‘$$b.   .e$*’     ‘$$$$$$$   '$$$c.dP  $$$$’  $$$’$$$$$$$P\r\n    ‘*$$$*’           ‘          *$$*’   ‘$*    ‘$’ ^$* ‘$’\r\n");
+
         }
     }
 }
